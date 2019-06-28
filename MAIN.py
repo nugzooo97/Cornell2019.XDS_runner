a71f1805f1b68fed8bc4e3aced36e5cf8797aadb
Aaron Finke <af554@mc224.lns.cornell.edu>
2019-06-28 09:53:41 -0400

    changed directory creation, added .gitignore

--- MAIN.py
+++ MAIN.py
@@ -1,46 +1,50 @@
-def main():
-	
-	parser = argparse.ArgumentParser(description='Arguments required to process the data: input, beamcenter, distance.')
-	
-	parser.add_argument('-i', '--input', type=str, nargs='+', required=True, help='Path of Directory containing HDF5 master file(s)')
-	
-	parser.add_argument('-b', '--beamcenter', type=int, nargs=2, required=True, help='Beam center in X and Y')
-	
-	parser.add_argument('-r', '--oscillations', type=float, default=1, help='Oscillation angle per well')
-	
-	parser.add_argument('-d', '--distance', type=int, required=True, help='Detector distance in mm')
-	
-	parser.add_argument('-w', '--wavelength', type=float, default=1.216, help='Wavelength in Angstrom')
-	
-	parser.add_argument('-f', '--framesperdegree', type=int, default=5, help='Number of frames per degree')
-	
-	parser.add_argument('-t', '--totalframes', type=int, default=0, help='Total number of frames to be processed')
-	
-	parser.add_argument('--output', default=os.getcwd(), help='Use this option to change output directly')
-	
-	parser.add_argument('-sg', '--spacegroup', help='Space group')
-	
-	parser.add_argument('-u', '--unitcell', type=str, default="100 100 100 90 90 90", help='Unit cell')
-	
-	args = parser.parse_args()
-	
+import argparse, fnmatch, os, h5py
+import master
 
-	
+def main():
+
+	parser = argparse.ArgumentParser(description='Arguments required to process the data: input, beamcenter, distance.')
+
+	parser.add_argument('-i', '--input', type=str, nargs='+', required=True, help='Path of Directory containing HDF5 master file(s)')
+
+	parser.add_argument('-b', '--beamcenter', type=int, nargs=2, required=True, help='Beam center in X and Y')
+
+	parser.add_argument('-r', '--oscillations', type=float, default=1, help='Oscillation angle per well')
+
+	parser.add_argument('-d', '--distance', type=float, required=True, help='Detector distance in mm')
+
+	parser.add_argument('-w', '--wavelength', type=float, default=1.216, help='Wavelength in Angstrom')
+
+	parser.add_argument('-f', '--framesperdegree', type=int, default=5, help='Number of frames per degree')
+
+	parser.add_argument('--output', default=os.getcwd(), help='Use this option to change output directly')
+
+	parser.add_argument('-sg', '--spacegroup', help='Space group')
+
+	parser.add_argument('-u', '--unitcell', type=str, default="100 100 100 90 90 90", help='Unit cell')
+
+	parser.parse_args()
+
+	args = parser.parse_args()
+
+
 	# We are going through each data file and creating a list called 'master_list' (defined in filter_master.py)
 	# The list will store all master files
-	
+
 	for masterdir in args.input:
 		master_list = fnmatch.filter(os.listdir(masterdir), "*master.h5")
+		print(master_list)
 		# Each element of the list now used to create an instance of a 'Master' class (defined in Master.py)
 		for masterfile in master_list:
 			# Return number of data files linked to a master file:
-			print(masterfile)
-			f = h5py.File("{a}/{b}".format(a=masterdir, b=masterfile))
-			num_of_total_frames = len(f['/entry/data'])
-			
-			master_class = Master(args, masterfile, num_of_total_frames)
+			masterpath = "{}{}".format(masterdir, masterfile)
+			parser.add_argument("-t","--totalframes", type=int, default=master.getNumberOfFiles_fast(masterpath),
+			help="Total number of frames to be processed, default all")
+			args = parser.parse_args()
+
+			master_class = master.Master(args, masterpath, args.totalframes)
 			master_class.create_and_run_Data_Wells()
-		# We just generated empty directory(ies) named with respect to the master(s) files, 
+		# We just generated empty directory(ies) named with respect to the master(s) files,
 		# AND returned the path to these directories
 
 if __name__=='__main__':
--- master.py
+++ master.py
@@ -1,5 +1,6 @@
-import os, sys
+import os, sys, h5py
 from datawell import Datawell
+
 
 class Master(object):
 
@@ -17,14 +18,15 @@ class Master(object):
 #		self.sg = args.spacegroup
 #		self.uc = args.unitcell
 
-		
+
 		self.create_master_directory()
 
 
 	def create_master_directory(self):
 		try:
 			end_index = self.masterpath.find('_master.h5')
-			dir_name = self.masterpath[:end_index]
+			start_index = self.masterpath.rfind('/')
+			dir_name= self.masterpath[start_index+1:end_index]
 			new_dir_path = '{new_dir}/{name}'.format(new_dir = self.output, name = dir_name)
 			try:
 				os.makedirs(new_dir_path)
@@ -34,15 +36,29 @@ class Master(object):
 				print("Successfully created the directory {}".format(dir_name))
 		except:
 			print("Something is not working. Check the code in 'Master.py'")
-	
+
 	def get_master_directory_path(self):
 		end_index = self.masterpath.find('_master.h5')
-		dir_name = self.masterpath[:end_index]
-		new_dir_path = '{new_dir}/{name}'.format(new_dir = self.output, name = dir_name)
-		return new_dir_path
-	
+		start_index = self.masterpath.rfind('/')
+		dir_name= self.masterpath[start_index+1:end_index]
+		return '{new_dir}/{name}'.format(new_dir = self.output, name = dir_name)
+
 	def create_and_run_Data_Wells(self):
-		dir_path = self.get_master_directory_path()
+#		dir_path = self.get_master_directory_path()
 		for framenum in range(1,self.total_frames,self.frames_per_degree):
-			data_well = Datawell(framenum, framenum+self.frames_per_degree-1, dir_path, self.masterpath, self.args)
+			data_well = Datawell(framenum, framenum+self.frames_per_degree-1, self.get_master_directory_path(), self.masterpath, self.args)
 			data_well.setup_datawell_directory()
+
+def get_h5_file(path):
+    try:
+        file = h5py.File(path, 'r')
+    except Exception:
+        raise IOError("Not a valid h5 file")
+    return file
+
+def getNumberOfFiles_fast(path):
+    """return number of data files by simply
+    finding the length of the H5 group. No checks."""
+    print(path)
+    f = get_h5_file(path)
+    return len(f['/entry/data'])
